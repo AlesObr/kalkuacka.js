1be7f99cbb08f9e032ebab226af41ecf618820b5
Aleš Obr <obr@spstrutnov.cz>
2022-11-28 09:35:24 +0100

    funkční základní operace

--- www/script.js
+++ www/script.js
@@ -3,10 +3,92 @@ var number2 = ""
 var operation = "";
 
 function addNumber(val){
-    console.log(val)
     val = String(val)
-    number1 = number1 + val
-    document.getElementById("main").innerHTML = number1;
+    if(operation == ""){
+        number1 = number1 + val
+        document.getElementById("main").innerHTML = number1;
+    }
+    else{
+        number2 = number2 + val
+        document.getElementById("main").innerHTML = number2;
+    }
 }
 
-function secti(operation)
+function doOperation(){
+    switch(operation){
+        case "minus":
+            number1 = Number(number1) - Number(number2);
+            document.getElementById("sub").innerHTML = number1;
+            number2 = "";
+            break;
+        case "plus":
+            number1 = Number(number1) + Number(number2);
+            document.getElementById("sub").innerHTML = number1;
+            number2 = "";
+            break;
+        case "krat":
+            number1 = Number(number1) * Number(number2);
+            document.getElementById("sub").innerHTML = number1;
+            number2 = "";
+            break;
+        case "deleno":
+            number1 = Number(number1) / Number(number2);
+            document.getElementById("sub").innerHTML = number1;
+            number2 = "";
+            break;
+}
+}
+
+function secti(){
+    if(operation == ""){
+        operation = "plus";
+    }
+    else{
+        doOperation();  
+    }
+}
+
+function odecti(){
+    if(operation == ""){
+        operation = "minus";
+    }
+    else{
+        doOperation();  
+    }
+}
+
+function nasob(){
+    if(operation == ""){
+        operation = "krat";
+    }
+    else{
+        doOperation();  
+    }
+}
+
+function del(){
+    if(operation == ""){
+        operation = "deleno";
+    }
+    else{
+        doOperation();  
+    }
+}
+
+function res(){
+    doOperation();
+    number1 = "";
+    document.getElementById("main").innerHTML = number1;
+    operation = "";
+}
+
+function vymazNum2(){
+    number2 = ""
+}
+
+function vymaz(){
+    number1 = "";
+    number2 = ""
+    document.getElementById("sub").innerHTML = number1;
+    document.getElementById("main").innerHTML = number2;
+}
